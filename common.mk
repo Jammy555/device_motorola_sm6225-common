f8029a00a1bbb312a594e3fe276b693439b5ce13
Prathap <prathapvenkatas@gmail.com>
2026-07-22 16:39:54 +0000

    Revert "sm6225-common: Init changes for Evolution-X"
    This reverts commit 3125af99cde3c0fc273a68500bb12166e3bed981.

--- common.mk
+++ common.mk
@@ -145,9 +145,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FM2
 
-# GApps
-TARGET_USES_MINI_GAPPS := true
-
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti \
@@ -252,7 +249,6 @@ PRODUCT_PACKAGES += \
     LineageApertureAppCommon \
     NcmTetheringOverlay \
     SettingsProviderResCommon \
-    SettingsResCommon \
     SystemUIResCommon \
     TelephonyResCommon \
     TelephonyResProduct \
